6b52c5f403c85bd6445c0d37ba99dd18a4c96380
MelikKtly <melikatilla@hotmail.com>
2024-10-04 18:05:16 +0300

    Deneme

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  standalone: true,
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  standalone: true,
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  currentYear: number;
 
+  constructor() {
+    this.currentYear = new Date().getFullYear(); // Mevcut yılı alır
+  }
 }
